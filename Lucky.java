c8fdc28757be21b9f1b2d3d6eb6112c8971582df
denis <kuchukovdenis2003@mail.ru>
2022-11-21 09:26:59 +0500

    Кучуков Денислам. Модуль 11.1

---
+++ Lucky.java
@@ -0,0 +1,41 @@
+import java.io.PrintStream;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Lucky {
+    static AtomicInteger luckyX = new AtomicInteger(0);
+    static AtomicInteger count = new AtomicInteger(0);
+
+    public Lucky() {
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread t1 = new LuckyThread();
+        Thread t2 = new LuckyThread();
+        Thread t3 = new LuckyThread();
+        t1.start();
+        t2.start();
+        t3.start();
+        t1.join();
+        t2.join();
+        t3.join();
+        System.out.println("Total: " + count);
+    }
+
+    static class LuckyThread extends Thread {
+        LuckyThread() {
+        }
+
+        public void run() {
+            while (Lucky.luckyX.get() < 999999) {
+                int x = Lucky.luckyX.incrementAndGet();
+                if (x % 10 + x / 10 % 10 + x / 100 % 10 == x / 1000 % 10 + x / 10000 % 10 + x / 100000 % 10) {
+                    PrintStream var10000 = System.out;
+                    String var10001 = Thread.currentThread().getName();
+                    var10000.println(var10001 + ": " + x);
+                    Lucky.count.incrementAndGet();
+                }
+            }
+
+        }
+    }
+}
